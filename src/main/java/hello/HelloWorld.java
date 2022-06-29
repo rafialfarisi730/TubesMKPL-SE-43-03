529177cc7a41f22737e60f642365edd7be5b6f14
FlazeFy <newbie8801@gmail.com>
2022-06-29 16:06:49 +0700

    update sonar

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,6 +6,11 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("Waktu menunjukan pukul: " + currentTime);
+          System.out.println("Waktu menunjukan pukul: " + currentTime);
+          System.out.println("Waktu menunjukan pukul: " + currentTime);
+          System.out.println("Waktu menunjukan pukul: " + currentTime);
+          System.out.println("Waktu menunjukan pukul: " + currentTime);
+          
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
